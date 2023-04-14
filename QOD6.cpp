82cea37f9eac317a6001f4a2be7a355895bcee49
Helazhary <98816029+Helazhary@users.noreply.github.com>
2023-04-14 19:43:04 +0200

    Add files via upload

---
+++ QOD6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int balanced_parentheses(string s)
+{
+    int longest = 0;
+    int count = 0;
+    for (int i = 0; i < s.length() - 1; i++)
+    {
+        if (s.substr(i, 2) == "()")
+        {
+            count++;
+            i++;
+        }
+        else
+        {
+            longest = max(longest, count);
+            count = 0;
+        }
+        longest = max(longest, count);
+    }
+    return 2 * longest;
+}
+
+int main()
+{
+    string s = "()((()()())()()()())()(()))";
+    cout << balanced_parentheses(s) << "\n";
+}
